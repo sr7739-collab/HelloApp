2303556b0f8b2f5292f5471e7cbdf975eadc9af6
sr7739-collab <sr7739@srmist.edu.in>
2026-03-03 09:06:46 +0530

    Create OOPSBANNERUC6.java

---
+++ OOPSBANNERUC6.java
@@ -0,0 +1,31 @@
+public class OOPSBannerApp {
+    
+    // Helper method to generate line segments for 'O' character
+    public static String[] generateO() {
+        return new String[] {
+            "    ***   ",
+            "  **   ** ",
+            " **     **",
+            " **     **",
+            " **     **",
+            " **     **",
+            " **     **",
+            "  **   ** ",
+            "    ***   "
+        };
+    }
+    
+    // Helper method to generate line segments for 'P' character
+    public static String[] generateP() {
+        return new String[] {
+            "*****  ",
+            "**   ** ",
+            "**   ** ",
+            "**   ** ",
+            "*****   ",
+            "**       ",
+            "**       ",
+            "**       ",
+            "**       "
+        };
+    }
